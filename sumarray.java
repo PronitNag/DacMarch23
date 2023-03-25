78aa907ffaefe681f03b304b4ef8b70c9bac3357
PronitNag <128138759+PronitNag@users.noreply.github.com>
2023-03-25 08:24:31 +0530

    Add files via upload

---
+++ sumarray.java
@@ -0,0 +1,10 @@
+class sumarray{
+	public static void main(String[] args){
+		int pro[]={4,5,6,7,8};
+		int sum=0;
+		for(int element:pro){
+			sum=sum+element;
+		}
+		System.out.println(sum);
+	}
+}
